4fcbcf17917755111a4b4f1bd0b265ce20120dba
wuhaha8080 <wuhaha8080@126.com>
2017-12-23 12:31:31 +0800

    E 1st commit

--- src/GitOne/gitOne.java
+++ src/GitOne/gitOne.java
@@ -4,5 +4,6 @@ public class gitOne {
 public static void main(String[] args) {
 	System.out.println("A 1st commit");
 	System.out.println("D 1st commit");
+	System.out.println("E 1st commit");
 }
 }
